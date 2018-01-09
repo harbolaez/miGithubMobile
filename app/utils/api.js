f2d0d505d484345d422d52bf052fc3df7e78ffcb
Henry Arbolaez <henryarbolaez@gmail.com>
2018-01-08 23:32:41 -0500

    Added Util folder for API

---
+++ app/utils/api.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const ROOT_URL = `https://api.github.com/users`;
+const REPO_URL = `https://api.github.com/repos`
+
+export const getProfile = (username) => axios.get(`${ROOT_URL}/${username}`);
+
+export const getRepos   = (username, limit=100) => axios.get(`${ROOT_URL}/${username}/repos?per_page=${limit}&sort=stars`);
+
+export const getStarCount = (repos) => {
+  return repos.data.reduce((count, repo) => {
+    return count + repo.stargazers_count;
+  },0);
+}
+
+export const getSpecificRepo = ( repos, repo_id ) => {
+  return repos.filter((repo) => repo.id === repo_id )
+}
+
+export const getRepoInfo = (username, repo_name, type) => {
+  console.log(`${REPO_URL}/${username}/${repo_name}/${type}`)
+  return axios.get(`${REPO_URL}/${username}/${repo_name}/${type}`)
+}
+
+export const bytesToSize = (bytes) => {
+   var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+   if (bytes == 0) return '0 Byte';
+   var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+   return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+};
